79c25d9b93da160ed1f9be516ea7f8f086203c0c
suminyol <sumi786nyol@gmail.com>
2025-03-18 04:35:56 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score= true
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc"=> NaN
+// true=>1 , false=>0
+
+let isloggedin = "S"
+
+let booleanisloggedin= Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+// ""=>false, "S"=>true
+// 1=>true, 0=>false
+
+let somenumber=33
+let stringnumber= String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
